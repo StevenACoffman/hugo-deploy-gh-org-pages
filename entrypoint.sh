06e683d6fdb9f96dacb6b489bfb5090d2e6d73ea
Steve Coffman <steve.coffman@ithaka.org>
2019-08-08 18:11:56 -0400

    Standardize message

--- entrypoint.sh
+++ entrypoint.sh
@@ -57,7 +57,6 @@ git pull origin master
 cd "$GITHUB_WORKSPACE" || exit 1
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
-
 hugo
 
 # Go To Public folder
@@ -68,10 +67,7 @@ git add .
 
 # Commit changes.
 msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
-git commit -m "$msg"
+git commit -am "$msg"
 
 # Push source and build repos.
 git push origin master
